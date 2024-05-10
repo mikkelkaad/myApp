cc1e17107a5dbee8a61d4b6ead999b31264761bb
mikkelkaad <mikkelkaad@gmail.com>
2024-05-10 16:20:09 +0200

    Added Swal input for pet weight,birthday and name

--- client/src/components/Animals.jsx
+++ client/src/components/Animals.jsx
@@ -1,38 +1,30 @@
 import { Card } from "./Card";
-import Swal from 'sweetalert2';
 import placeholder from './placeholder.png'
 import {getJson} from './getJson';
 import { BreedsModal } from "./BreedsModal";
 
 let breeds = await getJson('http://localhost:8080/breeds');
 breeds = breeds.data.result;
+let pet = {};
 
-const func = async (species, username)=>{
-    let pet = {};
+const registerPet = async (species, username)=>{
     pet.species = species;
     pet.user = username;
     BreedsModal.show("#dog");
-     
     // pet.name = prompt('What name?');
     // pet.birthday = prompt('What birthday?');
     // pet.weight = prompt('How much weight?');
-    
-    console.log(pet)
-    Swal.fire({icon:"success",
-        showConfirmButton:false,
-        timer:1000
-    });
 };
 
 export const Animals = (props)=>{
     return(
     <div className="App">
 
-        <BreedsModal breeds={breeds} modalId="dog"/>
+        <BreedsModal breeds={breeds} pet={pet} modalId="dog"/>
 
     <h1><code>Register a new Pet?</code></h1>
     <Card cardAlt="Picture of a cat" cardUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg"/>
-    <Card func={()=>{func("dog", props.user.username)}} cardAlt="Picture of a dog" cardUrl="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"/>
+    <Card func={()=>{registerPet("dog", props.user.username)}} cardAlt="Picture of a dog" cardUrl="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"/>
     <Card cardAlt="Picture for choosing 'other' pet type" cardUrl={placeholder} />
 
     <h1><code>Your pets</code></h1>
--- client/src/components/BreedsModal.jsx
+++ client/src/components/BreedsModal.jsx
@@ -1,11 +1,51 @@
 import "../styles/Modal.css";
 import {Card} from "./Card";
-import placeholder from "./placeholder.png"
+import placeholder from "./placeholder.png";
+import Swal from "sweetalert2";
+
+
+const validateInput = (name,weight,birthday)=>{
+    let isValid = true;
+    if(name === "") isValid = false;
+    if(weight === "") isValid = false;
+    if(birthday === "") isValid = false;
+    return isValid;
+}
+
+
+const breedPicker = (props,i)=>{
+    props.pet.breed = props.breeds[i].breed;
+    Swal.fire({
+        title:"We just need a bit more info about your pet:",
+        background:"#ff7f50",
+        color:"#000",
+        html:`
+        <input placeholder="Name" id="petName"  />
+        <input placeholder="Weight"id="petWeight" />
+        <input placeholder="Birthday" id="petBirtday" type="date"  />
+        `,
+        preConfirm:async()=>{
+            return validateInput(
+            document.getElementById('petName').value,
+            document.getElementById('petWeight').value,
+            document.getElementById('petBirtday').value   
+        )
+        }
+    }).then((result)=>{
+        if(!result.isConfirmed)return;
+        props.pet.name = document.getElementById('petName').value;
+        props.pet.weight = document.getElementById('petWeight').value;
+        props.pet.birtday = document.getElementById('petBirtday').value;
+        
+        
+        console.log(props.pet);
+    })
+}
 
 export const BreedsModal = (props)=>{
     let list = [];
     for(let i=0;i<props.breeds.length;i++){
-        list.push(<Card cardUrl={placeholder} cardAlt={props.breeds[i].breed} key={props.breeds[i].id} />);
+        list.push(<><Card cardUrl={placeholder} cardAlt={props.breeds[i].breed} key={props.breeds[i].id} func={()=>{breedPicker(props,i)}} /></>);
     }
     return(
         <div id ={props.modalId} className="modal">
